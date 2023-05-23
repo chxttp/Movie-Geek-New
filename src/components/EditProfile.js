be6eefca92c1132d765a79d44a8b46788fddbb69
Chayut Anegsiripong <chayut_henry@hotmail.com>
2023-05-24 03:40:24 +0700

    updated EditProfile pop page

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -4,6 +4,7 @@ import "./EditProfile.css";
 function EditProfile({ onClose }) {
   const [username, setUsername] = useState("nonnynon");
   const [bio, setBio] = useState("");
+  const [profilePicture, setProfilePicture] = useState(null);
   const fileInputRef = useRef(null);
 
   const handleUsernameChange = (event) => {
@@ -14,10 +15,13 @@ function EditProfile({ onClose }) {
     setBio(event.target.value);
   };
 
-  const handleProfilePictureUpload = (event) => {
+  const handleProfilePictureUpload = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleFileInputChange = (event) => {
     const file = event.target.files[0];
-    // Perform the file upload logic here
-    // For example, you can upload the file to a server using FormData or an API call
+    setProfilePicture(URL.createObjectURL(file));
   };
 
   const handleSubmit = (event) => {
@@ -26,9 +30,6 @@ function EditProfile({ onClose }) {
     // You can use the username and bio state values to update the user's profile information
     // For example, you can make an API call to update the user's profile data
     // Once the update is successful, you can close the EditProfileWidget or show a success message
-  };
-  const handleProfilePictureClick = () => {
-    fileInputRef.current.click();
   };
 
   return (
@@ -39,36 +40,40 @@ function EditProfile({ onClose }) {
             <h1>UPDATE PROFILE</h1>
           </div>
           <div className="form-group">
-            <label htmlFor="username">Username:</label>
+            <div className="pfp-container">
+              <div className="profile-picture-label">
+                <label htmlFor="profilePicture">Update Profile Picture</label>
+                <button onClick={handleProfilePictureUpload}>
+                  Choose a File
+                </button>
+                <input
+                  type="file"
+                  id="profilePicture"
+                  accept="image/*"
+                  onChange={handleFileInputChange}
+                  style={{ display: "none" }}
+                  ref={fileInputRef}
+                />
+              </div>
+              <div className="profile-picture-preview">
+                {profilePicture && (
+                  <img
+                    src={profilePicture}
+                    alt="Profile"
+                    className="profile-image"
+                  />
+                )}
+              </div>
+            </div>
+          </div>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
             <input
               type="text"
               id="username"
               value={username}
               onChange={handleUsernameChange}
             />
-          </div>
-          {/* <div className="form-group">
-              <label htmlFor="description">Description:</label>
-              <select>
-                <option value="">Artist</option>
-                <option value="">Reviewer</option>
-                <option value="">Actor</option>
-                <option value="">Actress</option>
-                <option value="">Influencer</option>
-              </select>
-            </div> */}
-
-          <div className="form-group">
-            <label htmlFor="profilePicture">Edit Profile Picture:</label>
-            <button onClick={handleProfilePictureUpload}>
-              Edit Picture
-              <input
-                type="file"
-                id="profilePicture"
-                accept="image/*"
-                onChange={handleProfilePictureUpload}
-              />
-            </button>
           </div>
           <div className="form-group">
             <label htmlFor="bio">Bio:</label>
